a6d669af50a51f9fbee856a1cc29172b3668a768
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2023-01-10 20:44:03 +0100

    Initiate FE rewrite
    Note: A strange permissions issue was detected. (13) Permission denied for the
    calendar.pl script. The weird part is that this never occured when using
    the old project and I really can't spot the difference right now.

---
+++ Koha/Plugin/Com/LMSCloud/RoomReservations/Models/calendar.pl
@@ -0,0 +1,32 @@
+#!/usr/bin/perl
+
+use utf8;
+use English qw( -no_match_vars );
+use Modern::Perl;
+
+use File::Basename;
+use File::Spec;
+use CGI qw ( -utf8 );
+
+use C4::Output qw( output_html_with_http_headers );
+use C4::Auth   qw( get_template_and_user );
+
+our $VERSION = '1.0.0';
+
+my @dirs = File::Spec->splitdir( dirname(__FILE__) );
+
+splice @dirs, -1;
+my $plugin_dir = File::Spec->catdir(@dirs);
+
+my $query = CGI->new;
+my ( $template, $borrowernumber, $cookie ) = get_template_and_user(
+    {   template_name   => $plugin_dir . '/views/opac/calendar.tt',
+        query           => $query,
+        type            => 'opac',
+        authnotrequired => 1,
+        is_plugin       => 1,
+    }
+);
+
+output_html_with_http_headers $query, $cookie, $template->output;
+
