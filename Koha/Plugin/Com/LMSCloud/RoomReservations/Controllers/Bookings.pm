8758f0a6c0565e6f59d8280982a82c06ddc1ed43
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2023-01-17 11:24:25 +0100

    Add validation for default_max_booking_time
    Additionally, extract the validation logic into separate helper methods

--- Koha/Plugin/Com/LMSCloud/RoomReservations/Controllers/Bookings.pm
+++ Koha/Plugin/Com/LMSCloud/RoomReservations/Controllers/Bookings.pm
@@ -127,54 +127,117 @@ sub delete {
 sub _check_and_save_booking {
     my ( $body, $c, $booking_id ) = @_;
 
-    #TODO: Also check against open_hours table and max bookable time for room if set, otherwise default
-
-    my $sql = SQL::Abstract->new;
     my $dbh = C4::Context->dbh;
     $dbh->begin_work;    # start transaction
 
-    # Get the maxbookabletime for the room in the booking
-    my ( $max_bookable_time_where, @max_bookable_time_bind ) = $sql->where( { roomid => $body->{'roomid'} } );
-    my $room_query = "SELECT maxbookabletime FROM $ROOMS_TABLE $max_bookable_time_where";
-    my $room_sth   = $dbh->prepare($room_query);
-    $room_sth->execute(@max_bookable_time_bind);
-    my ($max_bookable_time) = $room_sth->fetchrow_array;
+    my $sql = SQL::Abstract->new;
 
-    # Check if the difference between start and end times exceeds the maxbookabletime
-    my $start_time = Time::Piece->strptime( $body->{'start'}, '%Y-%m-%dT%H:%M' );
-    my $end_time   = Time::Piece->strptime( $body->{'end'},   '%Y-%m-%dT%H:%M' );
-    my $duration   = ( $end_time - $start_time )->minutes;
-
-    if ( $duration > $max_bookable_time ) {
+    if ( !_is_bookable_time( $body->{'roomid'}, $body->{'start'}, $body->{'end'} ) ) {
         $dbh->rollback;    # rollback transaction
         return $c->render( status => 400, openapi => { error => 'The booking exceeds the maximum allowed time for the room.' } );
     }
 
-    # Check if the institution is open during the booking time
-    my @wday_conversion_arr = ( 6, 0, 1, 2, 3, 4, 5 );
-    my ( $open_hours_where, @open_hours_bind ) = $sql->where(
-        {   -and => [
-                day   => $wday_conversion_arr[ Time::Piece->strptime( $body->{'start'}, '%Y-%m-%dT%H:%M' )->wday - 1 ],
-                start => { '<=' => Time::Piece->strptime( $body->{'end'},   '%Y-%m-%dT%H:%M' )->hms },
-                end   => { '>=' => Time::Piece->strptime( $body->{'start'}, '%Y-%m-%dT%H:%M' )->hms },
-            ]
-        }
-    );
-    my $open_hours_query = "SELECT COUNT(*) FROM $OPEN_HOURS_TABLE $open_hours_where";
-    my $open_hours_sth   = $dbh->prepare($open_hours_query);
-    $open_hours_sth->execute(@open_hours_bind);
-    my ($open_hours_count) = $open_hours_sth->fetchrow_array;
-
-    if ( $open_hours_count == 0 ) {
+    if ( !_is_open_during_booking_time( $body->{'start'}, $body->{'end'} ) ) {
         $dbh->rollback;    # rollback transaction
         return $c->render( status => 400, openapi => { error => 'The institution is closed during the selected time frame.' } );
     }
 
+    if ( _has_conflicting_booking( $body->{'roomid'}, $body->{'start'}, $body->{'end'}, $booking_id ) ) {
+        $dbh->rollback;    # rollback transaction
+        return $c->render( status => 400, openapi => { error => 'There is a conflicting booking.' } );
+    }
+
+    try {
+        my $booking = {
+            borrowernumber => $body->{'borrowernumber'},
+            roomid         => $body->{'roomid'},
+            start          => $body->{'start'},
+            end            => $body->{'end'},
+            blackedout     => $body->{'blackedout'} || 0
+        };
+
+        my ( $stmt, @bind ) =
+            defined $booking_id
+            ? $sql->update( $BOOKINGS_TABLE, $booking, { bookingid => $booking_id } )
+            : $sql->insert( $BOOKINGS_TABLE, $booking );
+        my $sth = $dbh->prepare($stmt);
+        $sth->execute(@bind);
+        $dbh->commit;    # commit transaction
+        return $c->render( status => defined $booking_id ? 200 : 201, openapi => $body );
+    }
+    catch {
+        $dbh->rollback;
+        $c->unhandled_exception($_);
+    };
+}
+
+sub _is_bookable_time {
+    my ( $room_id, $start_time, $end_time ) = @_;
+
+    my $sql = SQL::Abstract->new;
+    my $dbh = C4::Context->dbh;
+
+    # Get the maxbookabletime for the room
+    my ( $where, @bind ) = $sql->where( { roomid => $room_id } );
+    my $room_query = "SELECT maxbookabletime FROM $ROOMS_TABLE $where";
+    my $room_sth   = $dbh->prepare($room_query);
+    $room_sth->execute(@bind);
+    my ($max_bookable_time) = $room_sth->fetchrow_array;
+
+    # Check if the difference between start and end times exceeds the maxbookabletime
+    my $_start_time = Time::Piece->strptime( $start_time, '%Y-%m-%dT%H:%M' );
+    my $_end_time   = Time::Piece->strptime( $end_time,   '%Y-%m-%dT%H:%M' );
+    my $duration    = ( $_end_time - $_start_time )->minutes || $self->retrieve_data('default_max_booking_time');
+
+    return $duration <= $max_bookable_time;
+}
+
+sub _is_open_during_booking_time {
+    my ( $start_time, $end_time ) = @_;
+
+    my $sql = SQL::Abstract->new;
+    my $dbh = C4::Context->dbh;
+
+    # Time::Piece starts weeks on Sundays. To get around that, we use the wday method and
+    # subtract one from the return value (1 = Sunday, 1..7) and pick the respective entry
+    # from the conversion array to stay in the german/european notation.
+    use constant {
+        MONDAY    => 0,
+        TUESDAY   => 1,
+        WEDNESDAY => 2,
+        THURSDAY  => 3,
+        FRIDAY    => 4,
+        SATURDAY  => 5,
+        SUNDAY    => 6,
+    };
+    my @wday_conversion_arr = ( SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY );
     my ( $where, @bind ) = $sql->where(
-        {   roomid => $body->{'roomid'},
+        {   -and => [
+                day   => $wday_conversion_arr[ Time::Piece->strptime( $start_time, '%Y-%m-%dT%H:%M' )->wday - 1 ],
+                start => { '<=' => Time::Piece->strptime( $end_time,   '%Y-%m-%dT%H:%M' )->hms },
+                end   => { '>=' => Time::Piece->strptime( $start_time, '%Y-%m-%dT%H:%M' )->hms },
+            ]
+        }
+    );
+    my $open_hours_query = "SELECT COUNT(*) FROM $OPEN_HOURS_TABLE $where";
+    my $open_hours_sth   = $dbh->prepare($open_hours_query);
+    $open_hours_sth->execute(@bind);
+    my ($open_hours_count) = $open_hours_sth->fetchrow_array;
+
+    return $open_hours_count > 0;
+}
+
+sub _has_conflicting_booking {
+    my ( $room_id, $start_time, $end_time, $booking_id ) = @_;
+
+    my $sql = SQL::Abstract->new;
+    my $dbh = C4::Context->dbh;
+
+    my ( $where, @bind ) = $sql->where(
+        {   roomid => $room_id,
             -and   => [
-                start => { '<=' => $body->{'end'} },
-                end   => { '>=' => $body->{'start'} },
+                start => { '<=' => $end_time },
+                end   => { '>=' => $start_time },
             ]
         }
     );
@@ -182,50 +245,9 @@ sub _check_and_save_booking {
     my $check_query = "SELECT COUNT(*) FROM $BOOKINGS_TABLE $where";
     my $check_sth   = $dbh->prepare($check_query);
     $check_sth->execute(@bind);
-    my ($count) = $check_sth->fetchrow_array;
+    my ($conflict_count) = $check_sth->fetchrow_array;
 
-    if ( $count > 0 ) {
-        $dbh->rollback;    # rollback transaction
-        return $c->render( status => 400, openapi => { error => 'A booking already exists in the selected time frame.' } );
-    }
-
-    try {
-        my ( $query, $sth );
-        if ( defined $booking_id ) {
-            my ( $stmt, @bind ) = $sql->update(
-                $BOOKINGS_TABLE,
-                {   borrowernumber => $body->{'borrowernumber'},
-                    roomid         => $body->{'roomid'},
-                    start          => $body->{'start'},
-                    end            => $body->{'end'},
-                    blackedout     => $body->{'blackedout'} || 0
-                },
-                { bookingid => $booking_id }
-            );
-            $sth = $dbh->prepare($stmt);
-            $sth->execute(@bind);
-        }
-        else {
-            my ( $stmt, @bind ) = $sql->insert(
-                $BOOKINGS_TABLE,
-                {   borrowernumber => $body->{'borrowernumber'},
-                    roomid         => $body->{'roomid'},
-                    start          => $body->{'start'},
-                    end            => $body->{'end'},
-                    blackedout     => $body->{'blackedout'} || 0
-                }
-            );
-            $sth = $dbh->prepare($stmt);
-            $sth->execute(@bind);
-        }
-
-        $dbh->commit;    # commit transaction
-        return $c->render( status => ( defined $booking_id ) ? 200 : 201, openapi => $body );
-    }
-    catch {
-        $dbh->rollback;
-        $c->unhandled_exception($_);
-    };
+    return $conflict_count > 0;
 }
 
 1;
