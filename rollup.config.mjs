9f8abb5178d5f6731e080fc2ff6e2e04425a0bba
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2023-01-19 21:47:06 +0100

    Revert "Prepare for ts-migration"
    This reverts commit 9639a73cffaca70f17aabc8a04ba493fb5640a72.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,23 +1,20 @@
-import nodeResolve from "rollup-plugin-node-resolve";
-import commonjs from "rollup-plugin-commonjs";
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonjs from 'rollup-plugin-commonjs';
 import template from "rollup-plugin-html-literals";
+import minifyHTML from 'rollup-plugin-minify-html-literals';
 // import analyze from 'rollup-plugin-analyzer';
-import { terser } from "rollup-plugin-terser";
-import typescript from "rollup-plugin-typescript2";
+import { terser } from 'rollup-plugin-terser';
 
 export default [
   {
-    input: "./src/main.ts",
+    input: './src/main.js',
     output: {
-      dir: "./Koha/Plugin/Com/LMSCloud/RoomReservations/js/",
-      format: "umd",
-      name: "RoomReservationBundle",
+      dir: './Koha/Plugin/Com/LMSCloud/RoomReservations/js/',
+      format: 'umd',
+      name: 'RoomReservationBundle',
       sourcemap: true,
     },
     plugins: [
-      typescript({
-        tsconfig: "tsconfig.json",
-      }),
       nodeResolve(),
       commonjs(),
       template(),
---
+++ src/main.js
@@ -0,0 +1,60 @@
+import { html, LitElement } from 'lit';
+import LMSRoom from './components/LMSRoom';
+import LMSModal from './components/LMSModal';
+import LMSRoomModal from './components/LMSRoomModal';
+import LMSEquipmentItem from './components/LMSEquipmentItem';
+import LMSEquipmentModal from './components/LMSEquipmentModal';
+import LMSSearch from './components/LMSSearch';
+import LMSTable from './components/LMSTable';
+import LMSSettingsTable from './components/LMSSettingsTable';
+import LMSOpenHoursTable from './components/LMSOpenHoursTable';
+import LMSBookingsTable from './components/LMSBookingsTable';
+import LMSBookingsModal from './components/LMSBookingsModal';
+import LMSCalendar from './components/LMSCalendar/lib/lms-calendar';
+import LMSBookie from './components/LMSBookie';
+
+function renderOnUpdate({
+  entryPoint,
+  tagname,
+  eventName,
+  eventTarget,
+  endpoint,
+  options = {},
+}) {
+  const entryPointRef = entryPoint;
+  const eventTargetRef = eventTarget || entryPoint;
+  eventTargetRef.addEventListener(eventName, async () => {
+    const response = await fetch(endpoint, options);
+    if ([200, 201].includes(response.status)) {
+      const result = await response.json();
+      entryPointRef.innerHTML = '';
+      result.forEach((item) => {
+        const element = document.createElement(tagname);
+        Object.keys(item).forEach((key) => {
+          element.setAttribute(key, item[key]);
+        });
+        entryPointRef.appendChild(element);
+      });
+    }
+  });
+}
+
+
+export {
+  html,
+  LitElement,
+  LMSRoom,
+  LMSModal,
+  LMSRoomModal,
+  LMSEquipmentItem,
+  LMSEquipmentModal,
+  LMSSearch,
+  LMSTable,
+  LMSSettingsTable,
+  LMSOpenHoursTable,
+  LMSBookingsTable,
+  LMSBookingsModal,
+  LMSCalendar,
+  LMSBookie,
+  renderOnUpdate,
+};
