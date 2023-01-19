9639a73cffaca70f17aabc8a04ba493fb5640a72
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2023-01-19 21:24:54 +0100

    Prepare for ts-migration

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,20 +1,23 @@
-import nodeResolve from 'rollup-plugin-node-resolve';
-import commonjs from 'rollup-plugin-commonjs';
+import nodeResolve from "rollup-plugin-node-resolve";
+import commonjs from "rollup-plugin-commonjs";
 import template from "rollup-plugin-html-literals";
-import minifyHTML from 'rollup-plugin-minify-html-literals';
 // import analyze from 'rollup-plugin-analyzer';
-import { terser } from 'rollup-plugin-terser';
+import { terser } from "rollup-plugin-terser";
+import typescript from "rollup-plugin-typescript2";
 
 export default [
   {
-    input: './src/main.js',
+    input: "./src/main.ts",
     output: {
-      dir: './Koha/Plugin/Com/LMSCloud/RoomReservations/js/',
-      format: 'umd',
-      name: 'RoomReservationBundle',
+      dir: "./Koha/Plugin/Com/LMSCloud/RoomReservations/js/",
+      format: "umd",
+      name: "RoomReservationBundle",
       sourcemap: true,
     },
     plugins: [
+      typescript({
+        tsconfig: "tsconfig.json",
+      }),
       nodeResolve(),
       commonjs(),
       template(),
