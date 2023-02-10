44c3e54da2e8e35a84174fe55327ca51b79d15a5
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2023-02-10 18:12:17 +0100

    Change dynamically added strings
    This is necessary because without the language flags that come with
    their own problems it is not detected properly

---
+++ dynamically_added_strings.js
@@ -0,0 +1,25 @@
+/** Dynamically added Strings */
+
+/** This file is used to provide an entry point to xgettext for strings that come straight from an endpoint. */
+
+const __ = (string) => string;
+
+__('setting')
+__('value')
+__('description')
+__('bookingid')
+__('borrowernumber')
+__('roomid')
+__('start')
+__('end')
+__('blackedout')
+__('created')
+__('updated_at')
+__('day')
+__('Monday')
+__('Tuesday')
+__('Wednesday')
+__('Thursday')
+__('Friday')
+__('Saturday')
+__('Sunday')
