1f60035d9adecb21066fb00eb3c9ea827420c365
LMSCloudPaulD <paul.derscheid@lmscloud.de>
2023-02-12 18:47:16 +0100

    Update translations

--- dynamically_added_strings.js
+++ dynamically_added_strings.js
@@ -1,6 +1,7 @@
 /** Dynamically added Strings */
 
-/** This file is used to provide an entry point to xgettext for strings that come straight from an endpoint. */
+/** This file is used to provide an entry point to xgettext for strings that come straight from the database
+ *  which you obviously can't translate. */
 
 const __ = (string) => string;
 
@@ -13,6 +14,7 @@ __('roomid')
 __('start')
 __('end')
 __('blackedout')
+__('equipment')
 __('created')
 __('updated_at')
 __('day')
