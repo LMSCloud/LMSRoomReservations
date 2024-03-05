a58f5e63591804fc4754f34be01226bb0b9be2ce
Paul Derscheid <paul.derscheid@lmscloud.de>
2024-03-05 19:36:12 +0100

    chore: increment patch version

--- Koha/Plugin/Com/LMSCloud/RoomReservations.pm
+++ Koha/Plugin/Com/LMSCloud/RoomReservations.pm
@@ -21,7 +21,7 @@ use Try::Tiny  qw( catch try );
 use Koha::Plugin::Com::LMSCloud::Util::MigrationHelper ();
 
 ## Here we set our plugin version
-our $VERSION         = "4.8.1";
+our $VERSION         = "4.8.2";
 our $MINIMUM_VERSION = '21.05';
 
 ## Here is our metadata, some keys are required, some are optional
@@ -29,7 +29,7 @@ our $metadata = {
     name            => 'LMSRoomReservations',
     author          => 'LMSCloud GmbH',
     date_authored   => '2009-01-27',
-    date_updated    => "2024-03-01",
+    date_updated    => "2024-03-05",
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,
