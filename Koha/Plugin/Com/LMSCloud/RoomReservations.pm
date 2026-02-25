eeadd16a949c9b177e799eaff2d13fd0c15e40bd
Paul Derscheid <paul.derscheid@lmscloud.de>
2026-02-25 14:46:46 +0100

    chore: increment patch version to 5.3.16

--- Koha/Plugin/Com/LMSCloud/RoomReservations.pm
+++ Koha/Plugin/Com/LMSCloud/RoomReservations.pm
@@ -21,7 +21,7 @@ use Koha::Plugin::Com::LMSCloud::Util::MigrationHelper ();
 use Koha::Plugin::Com::LMSCloud::Util::Pages           qw( create_opac_page delete_opac_page page_exists get_page_url );
 
 ## Here we set our plugin version
-our $VERSION         = "5.3.1";
+our $VERSION         = "5.3.16";
 our $MINIMUM_VERSION = '22.11';
 
 ## Here is our metadata, some keys are required, some are optional
