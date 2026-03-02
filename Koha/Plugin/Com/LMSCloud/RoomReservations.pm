527ccc56d321e696f97ad71d895ad4adf439fccc
Paul Derscheid <paul.derscheid@lmscloud.de>
2026-03-02 13:59:32 +0100

    chore: bump minor version to 5.4.0

--- Koha/Plugin/Com/LMSCloud/RoomReservations.pm
+++ Koha/Plugin/Com/LMSCloud/RoomReservations.pm
@@ -21,7 +21,7 @@ use Koha::Plugin::Com::LMSCloud::Util::MigrationHelper ();
 use Koha::Plugin::Com::LMSCloud::Util::Pages           qw( create_opac_page delete_opac_page page_exists get_page_url );
 
 ## Here we set our plugin version
-our $VERSION         = "5.3.18";
+our $VERSION         = "5.4.0";
 our $MINIMUM_VERSION = '22.11';
 
 ## Here is our metadata, some keys are required, some are optional
@@ -29,7 +29,7 @@ our $metadata = {
     name            => 'LMSRoomReservations',
     author          => 'LMSCloud GmbH',
     date_authored   => '2009-01-27',
-    date_updated    => "2026-02-27",
+    date_updated    => "2026-03-02",
     minimum_version => $MINIMUM_VERSION,
     maximum_version => undef,
     version         => $VERSION,
